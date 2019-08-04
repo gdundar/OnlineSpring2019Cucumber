e5cef89d95d39558a03719710cca681fe1452cb3
gdundar <gokdar@gmail.com>
2019-08-04 18:05:25 -0500

    v.2

---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,39 @@
+package com.vytrack.step_definitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefinitions {
+
+    @Given("Open Vytrack login page")
+    public void open_Vytrack_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("Enter valid username and invalid password information")
+    public void enter_valid_username_and_invalid_password_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("Click login")
+    public void click_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("Message Invalid user name or password. should be displayed")
+    public void message_Invalid_user_name_or_password_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("Page title and url should be same")
+    public void page_title_and_url_should_be_same() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
